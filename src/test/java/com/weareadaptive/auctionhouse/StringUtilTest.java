884f92ea5ce9dfbd50d68f2a393a041b2f092027
Elisa <elisa.rotger@weareadaptive.com>
2023-11-21 13:14:46 +0100

    chore(test): unit tests for string util - alphanumeric

--- src/test/java/com/weareadaptive/auctionhouse/StringUtilTest.java
+++ src/test/java/com/weareadaptive/auctionhouse/StringUtilTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class StringUtilTest {
 
-  private static Stream<Arguments> testArguments() {
+  private static Stream<Arguments> testEmptyArguments() {
     return Stream.of(
         Arguments.of(null, true),
         Arguments.of("", true),
@@ -18,9 +18,26 @@ public class StringUtilTest {
     );
   }
 
+  private static Stream<Arguments> testAlphanumericArguments() {
+    return Stream.of(
+            Arguments.of("testing", true),
+            Arguments.of("testing675923", true),
+            Arguments.of("49203'4", true),
+            Arguments.of("    ", false),
+            Arguments.of("asa  asad", false),
+            Arguments.of("asl&&2", false)
+
+    );
+  }
+
   @ParameterizedTest(name = "{0} should return {1}")
-  @MethodSource("testArguments")
+  @MethodSource("testEmptyArguments")
   public void shouldTestStringIsNotNullOrBlank(String input, boolean expectedResult) {
     assertEquals(expectedResult, StringUtil.isNullOrEmpty(input));
+  }
+
+  @MethodSource("testAlphanumericArguments")
+  public void shouldTestStringIsAlphanumeric(String input, boolean expectedResult) {
+    assertEquals(expectedResult, StringUtil.isAlphanumeric(input));
   }
 }
